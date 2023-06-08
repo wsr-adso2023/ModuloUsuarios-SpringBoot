fb2f602e71a87bc2eacde6637950c59ea4317e61
wsr-adso2023 <basecentral2023@gmail.com>
2023-06-07 22:35:27 -0500

    Creacion de Proyecto con Arquitectura MVC

---
+++ src/test/java/com/modulousuario/ModuloUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
